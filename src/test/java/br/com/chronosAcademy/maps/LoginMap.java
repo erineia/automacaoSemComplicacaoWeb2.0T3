d14d171950cbd3e8cd506129056b4ae7a60e4387
erineia <erineiavs@gmail.com>
2022-09-01 20:22:35 -0300

    subindo aula 15 prefixos page e maps

--- src/test/java/br/com/chronosAcademy/maps/LoginMap.java
+++ src/test/java/br/com/chronosAcademy/maps/LoginMap.java
@@ -1,2 +1,15 @@
-package br.com.chronosAcademy.maps;public class LoginMap {
+package br.com.chronosAcademy.maps;
+
+import org.openqa.selenium.WebElement;
+
+public class LoginMap {
+    public WebElement btnLogin;
+    public WebElement btnFechar;
+    public WebElement divFecharModal;
+    public WebElement inpUserName;
+    public WebElement inpPassWord;
+    public WebElement btnSignIn;
+    public WebElement inpRemember;
+    public WebElement linkCreateAccout;
+
 }
--- src/test/java/br/com/chronosAcademy/pages/LoginPage.java
+++ src/test/java/br/com/chronosAcademy/pages/LoginPage.java
@@ -1,2 +1,51 @@
-package br.com.chronosAcademy.pages;public class LoginPage {
+package br.com.chronosAcademy.pages;
+
+import br.com.chronosAcademy.core.Driver;
+import br.com.chronosAcademy.maps.LoginMap;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    LoginMap loginMap;
+
+    public LoginPage() {
+        this.loginMap = loginMap;
+        PageFactory.initElements(Driver.getDriver(), loginMap);
+    }
+
+    public void clickBtnLogin(){
+        loginMap.btnLogin.click();
+    }
+
+    public void clickBtnFechar(){
+        loginMap.btnFechar.click();
+    }
+
+    public void clickDivFecharModal(){
+        loginMap.divFecharModal.click();
+    }
+
+    public void setInpUserName(String username){
+        loginMap.inpUserName.sendKeys(username);
+    }
+    public void setInpPassword(String password){
+        loginMap.inpPassWord.sendKeys(password);
+    }
+
+    public void clickInpRemember(){
+        loginMap.inpRemember.click();
+    }
+    public void clickLinkCreateAccout(){
+        loginMap.linkCreateAccout.click();
+    }
+
+    public void clickBtnSignIn(){
+        loginMap.btnSignIn.click();
+    }
+
+    public boolean isBtnSignIn(){
+        return loginMap.btnSignIn.isEnabled();
+    }
+
+
 }
